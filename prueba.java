3b0292973399c21261f9b511d0bcea23dea74e45
Miguel <2021206@iesluisbraille.es>
2022-06-15 19:59:23 +0200

    cambiado

--- prueba.java
+++ prueba.java
@@ -1,7 +1,7 @@
 public class S05Ejercicio01 {
  public static void main(String[] args) {
   
-   for(int i = 0; i <= 100; i += 5) {
+   for(int i = 0; i <= 200; i += 5) {
      System.out.println(i);
    }
  }
